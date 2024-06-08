1cfd10d2485b8fb45617f3bb7e67d80f7f2633b7
Fadahunsi-Paul <scandiclamar@gmail.com>
2024-06-08 02:38:15 -0700

    update

--- items/views.py
+++ items/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .tests  import Buyer
+from rest_framework import status
 
 @api_view(['GET','POST']) 
 @permission_classes([IsAuthenticated])
@@ -34,7 +35,24 @@ class ItemsList(APIView):
         return Response (serializer_class.data)
 
 class ItemListDetails(APIView):
+    # Read or list items
     def get(self,request,id):
         list_of_items = Items.objects.filter(item_id=id)
         serializer_class = ItemSerializer(list_of_items,many=True)
         return Response (serializer_class.data)
+
+    #Create an Item
+    def post(self,request):
+        serializer_obj = ItemSerializer(data=request.data)
+        if serializer_obj.is_valid(raise_exception=True):
+            saved_item = serializer_obj.save()
+            return Response("Success :" "Item '{}' created successfully".format(saved_item.name))
+        return Response(serializer_obj.errors,status=status.HTTP_400_BAD_REQUEST)    
+
+    # Update an item
+    def put(self,request,id):
+        item = Items.objects.get(item_id=id)
+        serializer_obj = ItemSerializer(data=request.data)
+        if serializer_obj.is_valid(raise_exception=True):
+            saved_item = serializer_obj.save()
+            return Response(f'{"Success : ""Item '{}' updated succesfully".format(saved_item.name)}')
